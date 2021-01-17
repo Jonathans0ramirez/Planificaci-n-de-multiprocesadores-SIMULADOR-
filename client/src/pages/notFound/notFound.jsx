cd741cd4e20c3c2c4c4c7024c58db592c2e67d2f
jonathans0ramirez <jonathan.ramirez@experimentality.co>
2021-01-17 10:38:40 -0500

    not found page with router

--- client/src/pages/notFound/notFound.jsx
+++ client/src/pages/notFound/notFound.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Result, Button } from 'antd';
+import { useHistory } from 'react-router-dom';
+import { HomeOutlined } from '@ant-design/icons';
 
 const NotFound = () => {
+    const history = useHistory();
+
     return (
-        <p>NOT FOUND 😣</p>
+        <Result
+            status="404"
+            title="404"
+            subTitle="La página no existe."
+            extra={
+                <Button
+                    type="primary"
+                    shape="round"
+                    icon={<HomeOutlined />}
+                    onClick={() =>
+                        history.replace("/")
+                    }
+                />
+            }
+        />
     )
 }
 
